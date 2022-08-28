7b5c2e825134a6a76de58f2aaa9b08cae1ca345e
twiceYuan <twiceyuan@gmail.com>
2022-08-28 09:36:40 +0800

    ref: 调整裁剪默认参数 - outputX、outputY 非必选，默认为 null

--- library/src/main/java/com/twiceyuan/imagepickcompat/options/CropOptions.kt
+++ library/src/main/java/com/twiceyuan/imagepickcompat/options/CropOptions.kt
@@ -7,8 +7,8 @@ package com.twiceyuan.imagepickcompat.options
  * Options about crop action.
  */
 data class CropOptions(
-    var outputX: Int = 600,
-    var outputY: Int = 600,
+    var outputX: Int? = null,
+    var outputY: Int? = null,
     var aspectX: Int = 1,
     var aspectY: Int = 1,
     var scale: Boolean = true,
